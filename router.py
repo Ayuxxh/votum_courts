79b3b3ff68b5c3518764617303b992b0be1da610
Tejasw Gupta <guptatejasw2002@gmail.com>
2026-03-26 17:49:33 +0530

    Fixes

--- router.py
+++ router.py
@@ -16,12 +16,12 @@ from .dc_services import \
 from .delhi_hc import get_delhi_case_details
 from .delhi_hc import \
     persist_orders_to_storage as delhi_persist_orders_to_storage
-from .DRT import (drt_get_details, drt_search_by_case_number,
-                  drt_search_by_diary_number, drt_search_by_party_name,
-                  drat_get_details, drat_search_by_case_number,
+from .DRT import (drat_get_details, drat_search_by_case_number,
                   drat_search_by_diary_number, drat_search_by_party_name,
-                  get_drat_case_types, get_drat_locations,
-                  get_drt_case_types, get_drt_locations)
+                  drt_get_details, drt_search_by_case_number,
+                  drt_search_by_diary_number, drt_search_by_party_name,
+                  get_drat_case_types, get_drat_locations, get_drt_case_types,
+                  get_drt_locations)
 from .DRT import persist_orders_to_storage as drt_persist_orders_to_storage
 from .gujarat_hc import (get_gujarat_case_details,
                          get_gujarat_case_details_by_cnr_no,
@@ -30,12 +30,12 @@ from .gujarat_hc import (get_gujarat_case_details,
                          gujarat_search_by_party_name)
 from .gujarat_hc import \
     persist_orders_to_storage as gujarat_persist_orders_to_storage
-from .jagriti import (get_jagriti_case_history, get_jagriti_case_status,
-                      get_jagriti_case_status_with_history,
-                      get_jagriti_commissions, get_jagriti_districts,
-                      get_jagriti_daily_order_judgement_pdf,
-                      jagriti_search_case_details,
-                      persist_orders_to_storage as jagriti_persist_orders_to_storage)
+# from .jagriti import (get_jagriti_case_history, get_jagriti_case_status,
+#                       get_jagriti_case_status_with_history,
+#                       get_jagriti_commissions, get_jagriti_districts,
+#                       get_jagriti_daily_order_judgement_pdf,
+#                       jagriti_search_case_details,
+#                       persist_orders_to_storage as jagriti_persist_orders_to_storage)
 from .hc_services import hc_get_benches, hc_get_case_types, hc_get_states
 from .NCLAT import (nclat_get_details, nclat_search_by_case_no,
                     nclat_search_by_free_text)
